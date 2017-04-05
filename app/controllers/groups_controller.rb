42b0dec77bff57bc00b6c8b1fa1d490119e99cd0
王天禹 <wandouba467@gmail.com>
2017-04-05 15:06:29 +0800

    implement groups function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,44 @@
 class GroupsController < ApplicationController
   def index
     @groups = Group.all
-  end 
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+
+    redirect_to groups_path
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    @group.update(group_params)
+
+    redirect_to groups_path, notice: "修改成功！"
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "影片已删除"
+    redirect_to groups_path
+   end
+   
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
