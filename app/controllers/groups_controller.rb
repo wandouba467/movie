52d86efe8a138d428af80529b7fe9c5a8dff5540
王天禹 <wandouba467@gmail.com>
2017-04-05 15:15:58 +0800

    add validation to title apply to new/edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,17 +17,23 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
 
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def update
     @group = Group.find(params[:id])
 
-    @group.update(group_params)
+     if @group.update(group_params)
 
     redirect_to groups_path, notice: "修改成功！"
+     else
+       render :edit
+     end
   end
 
   def destroy
@@ -36,7 +42,7 @@ class GroupsController < ApplicationController
     flash[:alert] = "影片已删除"
     redirect_to groups_path
    end
-   
+
   private
   def group_params
     params.require(:group).permit(:title, :description)
